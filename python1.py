23f40c46996fdad2c9f57fb66fef9961e61a3164
Dalek <abigailmarydelpiero@gmail.com>
2026-05-12 17:17:10 -0400

    change ig

--- python1.py
+++ python1.py
@@ -1,1 +1,8 @@
-print("hi")
+def are_you_playing_banjo(name):
+    l = name.lower()
+    x = (l.find("r"))
+    if x ==-1:
+        return name + " does not play banjo" 
+    else :
+        return name + "plays banjo"
+
